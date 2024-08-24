1991e000072a42ff83bde7aaefea7765a44203ff
yoru <2085509323@qq.com>
2024-08-25 02:56:21 +0800

    Core Java Learning Notes IDEA Project

---
+++ src/AdvancedFeatures/Chapter8/AnnotationTest.java
@@ -0,0 +1,29 @@
+package AdvancedFeatures.Chapter8;
+
+import java.lang.annotation.*;
+
+// 隐式拓展自Annotation接口: annotationType()方法返回Class描述注解对象
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR})  // 元注解：限制应用项
+@Retention(RetentionPolicy.RUNTIME)                     // 元注解：注解保留时长(默认CLASS类文件，SOURCE源代码，RUNTIME运行时)
+@Documented                                             // 元注解：指示归档工具收录该注解
+@Inherited                                              // 元注解：对类的注解，标识注解会被继承
+@Repeatable(AnnotationTests.class)                      // 元注解：可重复注解，需提供一个容器注解
+public @interface AnnotationTest {
+    int[] severity();
+
+    String assignedTo() default "[none]";
+
+    Class<?> testClass() default Void.class;    // 不允许为null
+
+    enum Status {FINE, BUG}
+
+    Status status() default Status.FINE;
+}
+
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@interface AnnotationTests {
+    AnnotationTest[] value();
+}
--- src/AdvancedFeatures/Chapter8/AnnotationUseTest.java
+++ src/AdvancedFeatures/Chapter8/AnnotationUseTest.java
@@ -1,16 +1,32 @@
 package AdvancedFeatures.Chapter8;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.junit.Test;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
+import javax.annotation.*;
+import javax.annotation.processing.Generated;
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+import java.util.*;
 
 public class AnnotationUseTest {
+    @Override                       // 用于编译的注解：注解覆盖方法
+    @Deprecated                     // 用于编译的注解：已过时标签
+    @SuppressWarnings("unchecked")  // 用于编译的注解：阻止特定的警告信息
+    @Generated("Unknown")           // 用于区分工具生成的源代码
+    @PostConstruct              // 用于管理资源的注解：对象构建后被调用
+    @PreDestroy                 // 用于管理资源的注解：对象销毁前被调用
+    @Resource(name = "mydb")    // 用于管理资源的注解：注入资源
+    public boolean equals(Object obj) {
+        return (List<@NonNull String>) obj == null;
+    }
+
     @Test(timeout = 10000)
     public void check() {
         System.out.println("checking...");
     }
 
+    @AnnotationTest(severity = {1, 2})   // 若是单值注解可以省略元素名
     public static void main(String[] args) throws ReflectiveOperationException {
         Method m = AnnotationUseTest.class.getDeclaredMethod("check");
         Annotation[] annotations = m.getDeclaredAnnotations();
@@ -18,5 +34,12 @@ public class AnnotationUseTest {
         if (annotation != null) {
             System.out.println(annotation.timeout());
         }
+        Arrays.stream(AnnotationUseTest.class
+                        .getDeclaredMethod("equals", Object.class)
+                        .getDeclaredAnnotations()
+                )
+                .map(Annotation::annotationType)
+                .map(Class::getName)
+                .forEach(System.out::println);
     }
 }
