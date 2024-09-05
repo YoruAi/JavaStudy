481779c90e33fce40bf623f1558f7df072cca663
yoru <2085509323@qq.com>
2024-09-05 22:52:20 +0800

    Core Java Learning Notes IDEA Project

--- src/AdvancedFeatures/Chapter5/JDBCTest.java
+++ src/AdvancedFeatures/Chapter5/JDBCTest.java
@@ -14,6 +14,7 @@ public class JDBCTest {
         DriverManager.setLogWriter(new PrintWriter(System.out));    // 获得跟踪信息到控制台
 
         try {
+            // 连接数据库（企业开发中更常用DataSource）
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db01",
                     "root", "111111");  // 最好使用try-with-resource
 
